3fbfb3a900fcf3f9c502946d2e5e8a7c4bb2204a
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-04-16 20:48:26 +0530

    updated java main class

--- src/Calculator.java
+++ src/Calculator.java
@@ -101,5 +101,10 @@ public class Calculator implements ActionListener{
 
     public void actionPerformed(ActionEvent e){
 
+        for(int i = 0; i<10; i++){
+            if(e.getSource() == numberButtons[i]){
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
+            }
+        }
     }
 }
